e07576679e48c0bc0b4cb8c143164af7f545f97a
Hamilton Hudson <ehamiltonhudson@gmail.com>
2019-03-15 14:28:32 -0400

    styling tweaks

--- pet-adopter/src/Welcome.js
+++ pet-adopter/src/Welcome.js
@@ -13,7 +13,7 @@ class Welcome extends Component {
         <header className="App-header">
           <img src="http://www.hslcnm.org/wp-content/uploads/2014/03/baby.jpg"
           alt="pet" className="App-logo"/>
-          <br/><br/><br/>
+          <br/><br/><br/><br/>
           <Animated animationIn="pulse" animationOut="flash" isVisible={true}>
             <p className="App-header-two">Adopt A Pet!</p>
           </Animated>
